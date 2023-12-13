e33c1548d8012384db186aee16812d7ad2d0a797
Ali Habibi <ali.habibi281@gmail.com>
2023-12-13 13:57:17 +0330

    feat(learn): add camelCase middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const queryString = require('querystring');
 const morgan = require('morgan');
+// const { default: camelCase } = require('camelcase-keys');
+const camelCaseKey = (...args) =>
+    import('camelcase-keys').then(({ default: camelCaseKeys }) => camelCaseKeys(...args));
 const app = express();
 
+app.use(morgan('combined'));
+
+async function camelCase(req, res, next) {
+    req.body = await camelCaseKey(req.body, { deep: true });
+    req.query = await camelCaseKey(req.query);
+    req.params = await camelCaseKey(req.params);
+    next()
+}
+app.use(camelCase)
 
 
-app.use(morgan('combined'))
+
 
 // body parser - middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-
+app.use(express.urlencoded({ extended: true }));
 
 function specialRoute(req, res, next) {
     console.log('special route');
@@ -29,8 +39,7 @@ function getTime(req, res, next) {
     req.time = Date.now();
     next();
 }
-app.use(getTime)
-
+app.use(getTime);
 
 app.use(function (req, res, next) {
     console.log('log1');
@@ -62,9 +71,20 @@ app.use(
 app.get('/', (req, res, next) => {
     res.status(200).send(req.url);
 });
-app.get('/users',specialRoute,checkAuth, (req, res, next) => {
+app.get('/users', specialRoute, checkAuth, (req, res, next) => {
     res.status(200).send(req.url);
 });
+
+app.get('/blogs', async (req, res) => {
+    res.send({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+    });
+});
+
+
+
 
 app.listen(3000, () => {
     console.log('server run on port 3000');
